e10fd358585234d8226550f1e3092391e022bb93
Alfredo Sirait <edo.sirait25@gmail.com>
2025-03-19 20:43:37 +0700

    add configuration production

--- frontend-image-uploader/src/app/features/services/image-service.ts
+++ frontend-image-uploader/src/app/features/services/image-service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  private API_URL = 'http://localhost:3000';
+  private API_URL = environment.API_URL;
 
   constructor(private http: HttpClient) { }
 
   uploadImage(file: File): Observable<any> {
+    console.log('API_URL', this.API_URL)
     const formData = new FormData();
     formData.append('image', file);
     return this.http.post(`${this.API_URL}/upload`, formData);
---
+++ frontend-image-uploader/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  API_URL: 'https://image-uploader-production-fcc9.up.railway.app'
+};
